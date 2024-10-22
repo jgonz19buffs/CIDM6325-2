b86aff53a4bbce900d569a0a883f48a9aaf59b9e
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-10-22 13:05:32 -0500

    extending the administration site with custom views

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -3,6 +3,7 @@ import datetime
 from django.contrib import admin
 from django.http import HttpResponse
 from django.utils.safestring import mark_safe
+from django.urls import reverse
 from .models import Order, OrderItem
 
 # Register your models here.
@@ -45,6 +46,10 @@ def export_to_csv(modeladmin, request, queryset):
     return response
 export_to_csv.short_description = 'Export to CSV'
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
@@ -58,7 +63,8 @@ class OrderAdmin(admin.ModelAdmin):
         'paid',
         order_payment,
         'created',
-        'updated'
+        'updated',
+        order_detail
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
