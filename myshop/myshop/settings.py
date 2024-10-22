d97bb6ae99bd9faa727ed97c106004eae427ebe0
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-10-22 07:16:20 -0500

    Building the payment process

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    'payment.app.PaymentConfig',
     'shop.apps.ShopConfig',
 ]
 
