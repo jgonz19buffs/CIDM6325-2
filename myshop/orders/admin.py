9fe6b9363ba324439bc1551494788b4c9e451223
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-10-22 13:43:31 -0500

    rendering PDF files

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -50,6 +50,11 @@ def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
@@ -64,7 +69,8 @@ class OrderAdmin(admin.ModelAdmin):
         order_payment,
         'created',
         'updated',
-        order_detail
+        order_detail,
+        order_pdf,
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
